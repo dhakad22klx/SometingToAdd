9f1933ed6878f5ddb0ba1869d7e68667eaed34f9
dhakad22klx <dhakad22klx.pvt@gmail.com>
2025-02-13 21:29:52 +0530

    Added something in Class 12 and 22

--- GolangMatt/golang.go
+++ GolangMatt/golang.go
@@ -57,25 +57,26 @@ Passed by reference : (everything a passed by values, its is descriptor that pla
 
 //--------- Go class 12 : Structs, Struct tags, and Json  ---------//
 
- You can not store address of map(&m) because map under the hood arranges itself if we do some 
+You can not store address of map(&m) because map under the hood arranges itself if we do some 
 insert operations.
- When we are dealing with map of structs, we generally create map of pointers to structs.
+Maps return a copy of the value, not a reference
+When we are dealing with map of structs, we generally create map of pointers to structs.
  
 
- Two structs types are compatible if : 
-   - fields have the same types and names
-   - in the same order 
-   - and the same tags(*) 
+Two structs types are compatible if : 
+- fields have the same types and names
+- in the same order 
+- and the same tags(*) 
 
- Anonymous structs are structurally compatible.
+Anonymous structs are structurally compatible.
 
- Types with different user-declared names are never compatible.
+Types with different user-declared names are never compatible.
 
- Names structs types are convertible (if they are structurally compatible).
+Names structs types are convertible (if they are structurally compatible).
 
- Tag differences dont prevent type conversion.
+Tag differences dont prevent type conversion.
 
- Structs are passed by values unless  a pointer is used.
+Structs are passed by values unless  a pointer is used.
 
  "dot" notation works well with values but works on pointers too , equivalent to (*a).copies++
 
@@ -87,19 +88,20 @@ insert operations.
 
 //--------- Go class 14 : References and Values semantics   ---------//
 
- Any struct with mutex must be passed by reference(if they are copied they dont work).
+Any struct with mutex must be passed by reference(if they are copied they dont work).
 
- If things to be shared then always pass a pointer.
+If things to be shared then always pass a pointer.
 
- Stack allocation is more efficient and go tries to allocate on stack(accessing a variable directly is more efficient than following a pointer).Accessing a dense sequence of data is more efficient  than sparse data(an array is faster than linked list).
+Stack allocation is more efficient and go tries to allocate on stack(accessing a variable directly is more efficient than following a pointer).
+Accessing a dense sequence of data is more efficient  than sparse data(an array is faster than linked list).
 
- It means that go prefers to allocate on stack but sometimes cant.(9:02 in lecture).
+It means that go prefers to allocate on stack but sometimes cant.(9:02 in lecture).
 
- Value returned by range is always a copy, we have to use index if we want to mutate
-  Element.
+Value returned by range is always a copy, we have to use index if we want to mutate Element.
  
- Anytime function mutates a slice thats passed in , we must return a copy.(thats because slice backing array may be reallocated to grow).
- Keeping a pointer to an element of slice is risky(as if you mutate slice by appending some elements it address may change due to reallocation).
+Anytime function mutates a slice thats passed in , we must return a copy.(thats because slice backing array may be reallocated to grow).
+
+Keeping a pointer to an element of slice is risky(as if you mutate slice by appending some elements it address may change due to reallocation).
 
 //--------- Go class 17 : OOPs ---------//
 
@@ -117,11 +119,21 @@ insert operations.
 
 //--------- Go class 19 : Composition  ---------//
 
- Composition is not a inheritence.
- We can embed a type into another type even if it is not a struct.
- If fields are get promoted then so do the methods.
+Composition is not a inheritence.
+We can embed a type into another type even if it is not a struct.
+If fields are get promoted then so do the methods.
+
+
+
+//--------- Go class 22 : Concurrency ---------//
  
-  
+Execution happens in some non-deterministic order 
+-Different behaviour on different run even with the same input.
+-Executes independently.
+
+Concurrency does not necessarily make the program faster, parallelism does. 
+We need Concurrency in order to enable parallelism. We have concurrent program, but parallelism happens at runtime if cpu has multiple cores.
+
 
 
 
